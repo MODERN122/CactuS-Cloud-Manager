776383bf76ab2235a15a613965112cf948dd59fb
GooDCactuS <Marvele98@gmail.com>
2019-07-29 20:19:46 +0400

    Some kind of refactoring

--- Cloud Manager/Managers/CloudDrive.cs
+++ Cloud Manager/Managers/CloudDrive.cs
@@ -1,25 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
-using System.IO.IsolatedStorage;
-using System.Reflection;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Threading;
-
-using Microsoft.Win32;
 
 namespace Cloud_Manager.Managers
 {
--- Cloud Manager/Managers/DropboxManager.cs
+++ Cloud Manager/Managers/DropboxManager.cs
@@ -13,7 +13,7 @@ namespace Cloud_Manager.Managers
 {
     class DropboxManager : CloudDrive
     {
-        static DropboxClient dbx;
+        static DropboxClient _dbx;
 
 
         public DropboxManager()
@@ -22,18 +22,18 @@ namespace Cloud_Manager.Managers
             {
                 byte[] key = new byte[stream.Length];
                 stream.Read(key, 0, key.Length);
-                dbx = new DropboxClient(Encoding.Default.GetString(key));
+                _dbx = new DropboxClient(Encoding.Default.GetString(key));
             }
         }
 
         static async Task Download(string name, string id)
         {
-            var items = await dbx.Files.ListFolderAsync(string.Empty, true);
+            var items = await _dbx.Files.ListFolderAsync(string.Empty, true);
             foreach (var item in items.Entries)
             {
                 if (item.IsFile && item.AsFile.Id == id)
                 {
-                    using (var response = await dbx.Files.DownloadAsync(item.PathDisplay))
+                    using (var response = await _dbx.Files.DownloadAsync(item.PathDisplay))
                     {
                         var s = response.GetContentAsByteArrayAsync();
                         s.Wait();
@@ -52,21 +52,18 @@ namespace Cloud_Manager.Managers
                 FileName = name,
                 Filter = "All files (*.*)|*.*"
             };
-            if (saveDialog.ShowDialog() == true)
-            {
-                string downloadFileName = saveDialog.FileName;
-                var task = Task.Run(() => Download(downloadFileName, id));
-                task.Wait();
-            }
+            if (saveDialog.ShowDialog() != true) return;
+
+            var downloadFileName = saveDialog.FileName;
+            var task = Task.Run(() => Download(downloadFileName, id));
+            task.Wait();
         }
 
-        static async Task Upload(string file, string content, string currentPath)
+        static async Task Upload(string file, string content)
         {
             using (var mem = new MemoryStream(File.ReadAllBytes(content)))
             {
-                string path = currentPath;
-
-                var updated = await dbx.Files.UploadAsync(
+                await _dbx.Files.UploadAsync(
                     file,
                     WriteMode.Overwrite.Instance,
                     body: mem);
@@ -81,14 +78,14 @@ namespace Cloud_Manager.Managers
             if (openFileDialog.ShowDialog() == true)
             {
                 string mimeType = "application/unknown";
-                string ext = System.IO.Path.GetExtension(openFileDialog.FileName).ToLower();
+                string ext = Path.GetExtension(openFileDialog.FileName).ToLower();
                 RegistryKey regKey = Registry.ClassesRoot.OpenSubKey(ext);
                 if (regKey != null && regKey.GetValue("Content Type") != null)
                     mimeType = regKey.GetValue("Content Type").ToString();
                 string fileName = openFileDialog.FileName;
                 fileName = fileName.Substring(fileName.LastIndexOf('\\', fileName.Length - 2) + 1);
                 fileName = curDir.Path + '/' + fileName;
-                var task = Task.Run(() => Upload(fileName, openFileDialog.FileName, curDir.Path)); ;
+                var task = Task.Run(() => Upload(fileName, openFileDialog.FileName)); ;
                 task.Wait();
             }
         }
@@ -97,13 +94,13 @@ namespace Cloud_Manager.Managers
         {
             string path = curDir.Path;
 
-            var list = dbx.Files.ListFolderAsync(string.Empty, true).Result;
+            var list = _dbx.Files.ListFolderAsync(string.Empty, true).Result;
             foreach (var item in cutFiles)
             {
                 foreach (var listItem in list.Entries)
                 {
                     if ((listItem.IsFile && listItem.AsFile.Id == item.Id) || (listItem.IsFolder && listItem.AsFolder.Id == item.Id))
-                        dbx.Files.MoveV2Async(listItem.PathDisplay, path + "/" + listItem.Name);
+                        _dbx.Files.MoveV2Async(listItem.PathDisplay, path + "/" + listItem.Name);
                 }
             }
         }
@@ -113,25 +110,25 @@ namespace Cloud_Manager.Managers
             string path = parentDir.Path;
             if (path == "/Dropbox")
             {
-                dbx.Files.CreateFolderV2Async("/" + name);
+                _dbx.Files.CreateFolderV2Async("/" + name);
             }
             else
             {
                 path = path.Substring(path.IndexOf("/") + 1);
                 path = path.Substring(path.IndexOf("/"));
-                dbx.Files.CreateFolderV2Async(path + "/" + name);
+                _dbx.Files.CreateFolderV2Async(path + "/" + name);
             }
         }
 
         public override void RemoveFile(ICollection<FileStructure> selectedFiles)
         {
-            var list = dbx.Files.ListFolderAsync(string.Empty, true);
+            var list = _dbx.Files.ListFolderAsync(string.Empty, true);
             foreach (var selectedItem in selectedFiles)
             {
                 foreach (var listItem in list.Result.Entries)
                 {
                     if ((listItem.IsFile && listItem.AsFile.Id == selectedItem.Id) || (listItem.IsFolder && listItem.AsFolder.Id == selectedItem.Id))
-                        dbx.Files.DeleteV2Async(listItem.PathDisplay);
+                        _dbx.Files.DeleteV2Async(listItem.PathDisplay);
                 }
             }
 
@@ -154,7 +151,7 @@ namespace Cloud_Manager.Managers
 
         public override void RenameFile(ICollection<FileStructure> selectedFiles, string newName)
         {
-            var list = dbx.Files.ListFolderAsync(string.Empty, true).Result;
+            var list = _dbx.Files.ListFolderAsync(string.Empty, true).Result;
             foreach (var listItem in list.Entries)
             {
                 if ((listItem.IsFile && listItem.AsFile.Id == selectedFiles.First<FileStructure>().Id)
@@ -164,7 +161,7 @@ namespace Cloud_Manager.Managers
                     path = path.Substring(0, path.LastIndexOf("/"));
                     if (listItem.Name.IndexOf('.') >= 0)
                         newName += listItem.Name.Substring(listItem.Name.LastIndexOf('.'));
-                    dbx.Files.MoveV2Async(listItem.PathDisplay, path + "/" + newName);
+                    _dbx.Files.MoveV2Async(listItem.PathDisplay, path + "/" + newName);
                 }
             }
         }
@@ -183,7 +180,7 @@ namespace Cloud_Manager.Managers
 
         private async Task GetFolderFiles(string path = "")
         {
-            ListFolderResult result = await dbx.Files.ListFolderAsync(path, true);
+            ListFolderResult result = await _dbx.Files.ListFolderAsync(path, true);
             folderResult = new List<Metadata>(result.Entries);
         }
     }
